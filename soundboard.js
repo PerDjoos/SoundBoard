1eb911d3351a773f1dd41f961379c911bae0c9c2
NSki2 <nicskinn@iu.edu>
2026-06-08 23:10:14 -0700

    buttons and hover

--- soundboard.js
+++ soundboard.js
@@ -12,7 +12,50 @@ var oof = new Audio('oof2.mp3')
 var piggy = new Audio('piggy2.mp3')
 var quack = new Audio('quack2.mp3')
 var slurp = new Audio('slurp2.mp3')
+var what = new Audio('what.mp3')
+var tvstatic = new Audio('tv-static.mp3')
 
 function playSound(sound){
     sound.play();
 }
+
+async function getRandomColor() {
+  const response = await fetch("https://api.gofakeit.com/funcs/hexcolor");
+  return await response.text();
+}
+
+
+// hover effect and color change function
+function colorChangeWithHover(button, color) {
+  button.style.backgroundColor = color;
+  button.style.borderColor = color;
+}
+
+
+// reset to default
+function resetHover(button) {
+  button.style.backgroundColor = "";
+  button.style.borderColor = "";
+}
+
+
+// hover function
+$("button").on("mouseenter", async function () {
+
+
+  const button = this;
+  const color = await getRandomColor();
+
+
+  // only apply if still hovering
+  if (button.matches(":hover")) {
+     colorChangeWithHover(button, color);
+  }
+
+
+});
+
+
+$("button").on("mouseleave", function () {
+  resetHover(this);
+});
